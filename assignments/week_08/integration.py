9f61bc91a3a5779ae39155038137f551c996f269
tylerrom <tylerromero@mail.adelphi.edu>
2026-04-13 11:06:49 -0400

    more work done

--- assignments/week_08/integration.py
+++ assignments/week_08/integration.py
@@ -1,15 +1,27 @@
-# Students should edit this Python file (just as you would a Python cell in a Jupyter Notebook)
-# this module will contain functions for numerical integration
+import numpy as np
+def trapezoidal_rule(f, a, b, N):
+    ''' Approximate the integral of f from a to b using the trapezoidal rule with N slices.'''
+    
+    h = (b - a) / N  # step size
+    x = np.linspace(a, b, N+1)  # N+1 points from a to b
+    y = f(x)  # evaluate function at all points
+    
+    # Trapezoidal rule: (h/2) * [f(a) + 2*sum(f(x_i)) + f(b)]
+    integral = (h/2) * (y[0] + 2*np.sum(y[1:-1]) + y[-1])
+    
+    return integral
 
 
-def trapezoidal_rule():
-    #write the arguments, docstring, and actual code of this function
 
-    return
-
-
-
-def simpsons_rule():
-    #write the arguments, docstring, and actual code of this function
-
-    return
+def simpsons_rule(f, a, b, N):
+    if N % 2 != 0:
+        raise ValueError("N must be even for Simpson's rule")
+    
+    h = (b - a) / N  # step size
+    x = np.linspace(a, b, N+1)  # N+1 points from a to b
+    y = f(x)  # evaluate function at all points
+    
+    # Simpson's rule: (h/3) * [f(a) + 4*sum(odd indices) + 2*sum(even indices) + f(b)]
+    integral = (h/3) * (y[0] + 4*np.sum(y[1:-1:2]) + 2*np.sum(y[2:-1:2]) + y[-1])
+    
+    return integral
